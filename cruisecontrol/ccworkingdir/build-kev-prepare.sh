a0fc760576a9654ef8afdd81bc1e086ac9beef20
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2008-04-10 14:33:04 +0000

    misc

--- cruisecontrol/ccworkingdir/build-kev-prepare.sh
+++ cruisecontrol/ccworkingdir/build-kev-prepare.sh
@@ -3,9 +3,13 @@
 CURRENTDIR=`pwd`
 cd checkout/kev
 svn up
+rm -rf buildDirectory
 mkdir buildDirectory
 mkdir buildDirectory/features
 mkdir buildDirectory/plugins
-cp -R *.feature buildDirectory/features/
+
+mv *.feature buildDirectory/features/
+cp -R * buildDirectory/plugins/
+cp -R buildDirectory/*.feature ./
 
 cd $CURRENTDIR
