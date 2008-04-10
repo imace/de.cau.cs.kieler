f0f74cd817e30567663b0f4f1acb9f7f313a5f33
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2008-04-10 15:00:44 +0000

    test version of pde build command

--- cruisecontrol/ccworkingdir/build-kev-prepare.sh
+++ cruisecontrol/ccworkingdir/build-kev-prepare.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# headless built skript for KEV
+# author: Hauke Fuhrmann, haf
+
+ECLIPSE=/home/java/eclipse_3.4
+
 CURRENTDIR=`pwd`
 cd checkout/kev
 echo "updating from SVN..."
@@ -14,8 +19,15 @@ mkdir buildDirectory/plugins
 echo "creating building dir structure..."
 mv *.feature buildDirectory/features/
 cp -R * buildDirectory/plugins/
-cp -R buildDirectory/*.feature ./
+cp -R buildDirectory/features/* ./
 
 echo "running PDE build..."
+ARGS="org.eclipse.core.launcher.Main"
+ARGS=$ARGS" -application org.eclipse.ant.core.antRunner"
+ARGS=$ARGS" -DbaseLocation=$ECLIPSE"
+ARGS=$ARGS" -Dbuildfile $ECLIPSE/plugins/org.eclipse.pde.buildorg.eclipse.pde.build_3.4.0.v20080206/scripts/build.xml"
+ARGS=$ARGS" -DbuildDirectory=$CURRENTDIR/kev-build"
+ARGS=$ARGS" -Dbuilder=$CURRENTDIR/checkout/kev/edu.unikiel.rtsys.kieler.kev.build"
+echo "java -cp $ECLIPSE/startup.jar $ARGS"
 
 cd $CURRENTDIR
