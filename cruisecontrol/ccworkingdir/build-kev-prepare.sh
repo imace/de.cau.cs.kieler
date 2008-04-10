441453a39add1d8d389ed71feb7c8f56b8197d29
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2008-04-10 14:45:22 +0000

    fixes, outputs

--- cruisecontrol/ccworkingdir/build-kev-prepare.sh
+++ cruisecontrol/ccworkingdir/build-kev-prepare.sh
@@ -2,14 +2,20 @@
 
 CURRENTDIR=`pwd`
 cd checkout/kev
-svn up --user tomcat5 --password edcvgz
+echo "updating from SVN..."
+svn up --username tomcat5 --password edcvgz
+
+echo "cleaning old build..."
 rm -rf buildDirectory
 mkdir buildDirectory
 mkdir buildDirectory/features
 mkdir buildDirectory/plugins
-  
+
+echo "creating building dir structure..."
 mv *.feature buildDirectory/features/
 cp -R * buildDirectory/plugins/
 cp -R buildDirectory/*.feature ./
+
+echo "running PDE build..."
 
 cd $CURRENTDIR
