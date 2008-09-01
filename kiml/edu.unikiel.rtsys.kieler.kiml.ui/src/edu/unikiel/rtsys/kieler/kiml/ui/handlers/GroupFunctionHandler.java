5c3853d63a5e0475c213d8feea298eb88ba03482
Arne Schipper <ars@informatik.uni-kiel.de>
2008-09-01 17:01:34 +0000

    started GEF handler for layout fixed several menu/popup things

--- kiml/edu.unikiel.rtsys.kieler.kiml.ui/src/edu/unikiel/rtsys/kieler/kiml/ui/handlers/GroupFunctionHandler.java
+++ kiml/edu.unikiel.rtsys.kieler.kiml.ui/src/edu/unikiel/rtsys/kieler/kiml/ui/handlers/GroupFunctionHandler.java
@@ -32,11 +32,16 @@ public class GroupFunctionHandler extends AbstractHandler implements IHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		/*
-		 * As this handler is activated from a menu, we need to get the active
-		 * menu selection. Just calling selection does not work when calling
-		 * from within a view, for example.
+		 * As this handler is activated from a menu, Emma needs to get the
+		 * active menu selection. Just calling selection does not work when
+		 * calling from within a view, for example. If called from a menu in the
+		 * menu bar - not a context menu - Emma needs to use the
+		 * currrentSelection.
 		 */
 		ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+		if (selection == null)
+			selection = HandlerUtil.getCurrentSelection(event);
+		
 		/*
 		 * Filter out ShapeNodeEditParts. According to the menu.extension in
 		 * plugin.xml it should just be ShapeNodeEditParts selected anyway, but
