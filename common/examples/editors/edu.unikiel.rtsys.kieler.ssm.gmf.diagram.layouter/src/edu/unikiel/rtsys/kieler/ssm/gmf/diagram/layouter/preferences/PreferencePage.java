7e5cd6cb9e9387178eed0902d1d1e0d23c43a367
Arne Schipper <ars@informatik.uni-kiel.de>
2008-11-07 15:16:10 +0000

    Share project "edu.unikiel.rtsys.kieler.ssm.gmf.diagram.layouter" into "http://tick/svn/kieler"

---
+++ common/examples/editors/edu.unikiel.rtsys.kieler.ssm.gmf.diagram.layouter/src/edu/unikiel/rtsys/kieler/ssm/gmf/diagram/layouter/preferences/PreferencePage.java
@@ -0,0 +1,22 @@
+package edu.unikiel.rtsys.kieler.ssm.gmf.diagram.layouter.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class PreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	@Override
+	protected void createFieldEditors() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
