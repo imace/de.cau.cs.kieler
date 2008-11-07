d391e4885db8e3149f81a2c199da6827d1253879
Miro Spoenemann <msp@informatik.uni-kiel.de>
2008-11-07 10:00:35 +0000

    added dataflow diagram layouter

---
+++ kodata/edu.unikiel.rtsys.kieler.dataflow.diagram.layout/src/dataflow/diagram/services/DataflowDiagramLayouter.java
@@ -0,0 +1,25 @@
+package dataflow.diagram.services;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+import edu.unikiel.rtsys.kieler.kiml.layout.KimlLayoutGraph.KLayoutGraph;
+import edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter;
+
+public class DataflowDiagramLayouter extends KimlAbstractLayouter {
+
+	protected void applyLayout() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected KLayoutGraph buildLayoutGraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected boolean init(IStructuredSelection selection, String commandId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
