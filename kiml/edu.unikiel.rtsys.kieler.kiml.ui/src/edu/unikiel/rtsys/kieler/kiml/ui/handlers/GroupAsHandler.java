0e48a2a97c1735c59a03da291eae9f3cf2755172
Arne Schipper <ars@informatik.uni-kiel.de>
2008-11-07 16:06:22 +0000

    make group handler do layout automatically

--- kiml/edu.unikiel.rtsys.kieler.kiml.ui/src/edu/unikiel/rtsys/kieler/kiml/ui/handlers/GroupAsHandler.java
+++ kiml/edu.unikiel.rtsys.kieler.kiml.ui/src/edu/unikiel/rtsys/kieler/kiml/ui/handlers/GroupAsHandler.java
@@ -6,15 +6,20 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.draw2d.Animation;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.CompartmentEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeNodeEditPart;
+import org.eclipse.gmf.runtime.notation.Diagram;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import edu.unikiel.rtsys.kieler.kiml.layout.KimlLayoutGraph.LAYOUTER_INFO;
 import edu.unikiel.rtsys.kieler.kiml.layout.KimlLayoutGraph.LAYOUT_TYPE;
+import edu.unikiel.rtsys.kieler.kiml.layout.services.DiagramLayouters;
+import edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter;
 import edu.unikiel.rtsys.kieler.kiml.layout.services.LayoutProviders;
 import edu.unikiel.rtsys.kieler.kiml.ui.ContributionItemGroupAs;
 import edu.unikiel.rtsys.kieler.kiml.ui.helpers.KimlGMFLayoutHintHelper;
@@ -81,24 +86,34 @@ public class GroupAsHandler extends AbstractHandler implements IHandler {
 		// just another sanity check if the right commandID
 		if (commandID
 				.equals("edu.unikiel.rtsys.kieler.kiml.ui.command.groupAs")) {
+
 			groupID = KimlGMFLayoutHintHelper
-			.generateLayoutGroupID(selectedShapeNodeEditParts);
+					.generateLayoutGroupID(selectedShapeNodeEditParts);
 			/*
 			 * if just one selected, assume it was some kind of compartment and
 			 * apply grouping on sub elements
 			 */
 			if (selectedShapeNodeEditParts.size() == 1) {
-				
+
 				KimlGMFLayoutHintHelper.setChildrenLayoutHint(
 						(ShapeNodeEditPart) selectedShapeNodeEditParts.get(0),
 						groupID, layoutType, layouterName);
 			}
 			/* more selected, apply grouping information to selected elements */
 			else if (selectedShapeNodeEditParts.size() > 1) {
-				
-				KimlGMFLayoutHintHelper.setLayoutHint(selectedShapeNodeEditParts, groupID,
-						layoutType, layouterName);
+
+				KimlGMFLayoutHintHelper.setLayoutHint(
+						selectedShapeNodeEditParts, groupID, layoutType,
+						layouterName);
 			}
+
+			IEditorPart editorPart = HandlerUtil.getActiveEditor(event);
+			String editorId = editorPart.getEditorSite().getId();
+			KimlAbstractLayouter diagramLayouter = DiagramLayouters
+					.getInstance().getDiagramLayouter(editorId);
+			Animation.markBegin();
+			diagramLayouter.layout(editorPart);
+			Animation.run(700);
 		}
 		return null;
 	}
