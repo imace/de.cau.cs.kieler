bb50c2131295b91ce20f323c8a075de96fe053db
Miro Spoenemann <msp@informatik.uni-kiel.de>
2008-11-10 16:15:53 +0000

    added 'apply layout' functionality for dataflow diagrams

--- kodata/edu.unikiel.rtsys.kieler.dataflow.diagram.layout/src/dataflow/diagram/services/DataflowDiagramLayouter.java
+++ kodata/edu.unikiel.rtsys.kieler.dataflow.diagram.layout/src/dataflow/diagram/services/DataflowDiagramLayouter.java
@@ -4,14 +4,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.gef.commands.CompoundCommand;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
+import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
+import org.eclipse.gef.requests.ChangeBoundsRequest;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderedShapeEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.BorderedBorderItemEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ITextAwareEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramCommandStack;
+import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
+import org.eclipse.gmf.runtime.diagram.ui.requests.SetAllBendpointRequest;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.ui.IEditorPart;
 
 import dataflow.diagram.edit.parts.*;
@@ -22,12 +31,15 @@ import edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter;
 
 /**
  * This layouter for dataflow diagrams performs the needed transformation
- * from diagram elements to the KIML Layout Graph structure.
+ * from diagram elements to the KIML Layout Graph structure and applies layout
+ * information from the layout engine.
+ * 
  * @author msp
  */
 public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 
 	private DataflowModelEditPart modelPart = null;
+	private KLayoutGraph layoutGraph = null;
 	
 	private Map<KNodeGroup, AbstractBorderedShapeEditPart> nodeGroup2BoxMapping = new HashMap<KNodeGroup, AbstractBorderedShapeEditPart>();
 	private Map<KEdge, ConnectionEditPart> edge2ConnectionMapping = new HashMap<KEdge, ConnectionEditPart>();
@@ -39,6 +51,11 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 	 * @see edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter#init(java.lang.Object)
 	 */
 	protected boolean init(Object target) {
+		nodeGroup2BoxMapping.clear();
+		edge2ConnectionMapping.clear();
+		port2BorderItemMapping.clear();
+		borderItem2PortMapping.clear();
+		
 		// find the dataflow model edit part depending on the type of input
 		if (target instanceof IEditorPart)
 		{
@@ -60,10 +77,15 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 	 * @see edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter#buildLayoutGraph()
 	 */
 	protected KLayoutGraph buildLayoutGraph() {
-		KLayoutGraph layoutGraph = KimlLayoutGraphFactory.eINSTANCE.createKLayoutGraph();
+		layoutGraph = KimlLayoutGraphFactory.eINSTANCE.createKLayoutGraph();
 		// the layout graph always has a top node
 		KNodeGroup topNode = KimlLayoutGraphFactory.eINSTANCE.createKNodeGroup();
 		layoutGraph.setTopGroup(topNode);
+		// set the top node's layout
+		KNodeGroupLayout topGroupLayout = KimlLayoutGraphFactory.eINSTANCE.createKNodeGroupLayout();
+		createLayout(topGroupLayout, modelPart.getFigure());
+		topGroupLayout.setInsets(KimlLayoutGraphFactory.eINSTANCE.createKInsets());
+		topNode.setLayout(topGroupLayout);
 		
 		buildLayoutGraphRecursively(modelPart.getChildren(), topNode);		
 		return layoutGraph;
@@ -74,29 +96,122 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 	 * @see edu.unikiel.rtsys.kieler.kiml.layout.services.KimlAbstractLayouter#applyLayout()
 	 */
 	protected void applyLayout() {
+		// prepare changes to GMF diagram
+		DiagramCommandStack commandStack = new DiagramCommandStack(null);
+		CompoundCommand compoundCommand = new CompoundCommand();
+		compoundCommand.setLabel("Dataflow Diagram Layout");
+		ScalableFreeformRootEditPart rootEditPart = (ScalableFreeformRootEditPart)modelPart.getRoot();
+		double zoomLevel = rootEditPart.getZoomManager().getZoom();
+		
 		// apply node layouts
 		for (KNodeGroup nodeGroup : nodeGroup2BoxMapping.keySet())
 		{
 			AbstractBorderedShapeEditPart boxEditPart = nodeGroup2BoxMapping.get(nodeGroup);
-			//boxEditPart.getLocation()
+			ChangeBoundsRequest changeBoundsRequest = new ChangeBoundsRequest(RequestConstants.REQ_MOVE);
+			changeBoundsRequest.setEditParts(boxEditPart);
+			
+			Dimension oldSize = boxEditPart.getFigure().getBounds().getSize();
+			Dimension newSize = kDimension2Dimension(nodeGroup.getLayout().getSize());
+
+			Dimension sizeDelta = newSize.getExpanded(oldSize.negate());
+			changeBoundsRequest.setResizeDirection(PositionConstants.CENTER);
+			changeBoundsRequest.setSizeDelta(sizeDelta.scale(zoomLevel));
+
+			Point oldLocation = boxEditPart.getFigure().getBounds().getLocation();
+			Point newLocation = kPoint2Point(nodeGroup.getLayout().getLocation());
+
+			Point moveDelta = newLocation.getTranslated(oldLocation.negate());
+			changeBoundsRequest.setMoveDelta(moveDelta.scale(zoomLevel));
+			
+			compoundCommand.add(boxEditPart.getCommand(changeBoundsRequest));
 		}
+		
+		// apply port layouts
+		for (KPort port : port2BorderItemMapping.keySet())
+		{
+			BorderedBorderItemEditPart borderItem = port2BorderItemMapping.get(port);
+			ChangeBoundsRequest changeBoundsRequest = new ChangeBoundsRequest(RequestConstants.REQ_MOVE);
+			changeBoundsRequest.setEditParts(borderItem);
+			
+			Point oldLocation = borderItem.getFigure().getBounds().getLocation();
+			Point newLocation = kPoint2Point(port.getLayout().getLocation());
+
+			Point moveDelta = newLocation.getTranslated(oldLocation.negate());
+			changeBoundsRequest.setMoveDelta(moveDelta.scale(zoomLevel));
+			
+			compoundCommand.add(borderItem.getCommand(changeBoundsRequest));
+		}
+		
+		// apply edge layouts
+		for (KEdge edge : edge2ConnectionMapping.keySet())
+		{
+			ConnectionEditPart connection = edge2ConnectionMapping.get(edge);
+			KEdgeLayout edgeLayout = edge.getLayout();
+			PointList pointList = new PointList();
+
+			// set start point
+			Point startPoint = kPoint2Point(edgeLayout.getSourcePoint());
+			pointList.addPoint(startPoint);
+
+			// set grid points
+			for (KPoint gridPoint : edgeLayout.getGridPoints()) {
+				Point point = kPoint2Point(gridPoint);
+				pointList.addPoint(point);
+			}
+
+			// set end point
+			Point endPoint = kPoint2Point(edgeLayout.getTargetPoint());
+			pointList.addPoint(endPoint);
+
+			// create request and add it
+			SetAllBendpointRequest request = new SetAllBendpointRequest(RequestConstants.REQ_SET_ALL_BENDPOINT, pointList);
+			compoundCommand.add(connection.getCommand(request));
+		}
+		
+		commandStack.execute(compoundCommand);
 	}
 	
 	/**
 	 * Sets the location and size properties of a shape layout.
 	 * @param shapeLayout layout object to be changed
-	 * @param shapeEditPart source object with layout information
+	 * @param figure source object with layout information
 	 */
-	private static void createLayout(KShapeLayout shapeLayout, ShapeEditPart shapeEditPart)
+	private static void createLayout(KShapeLayout shapeLayout, IFigure figure)
 	{
 		KPoint kPoint = KimlLayoutGraphFactory.eINSTANCE.createKPoint();
-		kPoint.setX(shapeEditPart.getLocation().x);
-		kPoint.setY(shapeEditPart.getLocation().y);
+		kPoint.setX(figure.getBounds().x);
+		kPoint.setY(figure.getBounds().y);
 		shapeLayout.setLocation(kPoint);
 		KDimension kDimension = KimlLayoutGraphFactory.eINSTANCE.createKDimension();
-		kDimension.setHeight(shapeEditPart.getSize().height);
-		kDimension.setWidth(shapeEditPart.getSize().width);
+		kDimension.setHeight(figure.getBounds().height);
+		kDimension.setWidth(figure.getBounds().width);
 		shapeLayout.setSize(kDimension);
+	}
+	
+	/**
+	 * Converts a dimension from the KLayoutGraph into a Draw2D dimension.
+	 * @param kDimension KLayoutGraph dimension
+	 * @return Draw2D dimension
+	 */
+	private static Dimension kDimension2Dimension(KDimension kDimension)
+	{
+		Dimension dimension = new Dimension();
+		dimension.height = (int)kDimension.getHeight();
+		dimension.width = (int)kDimension.getWidth();
+		return dimension;
+	}
+	
+	/**
+	 * Converts a point from the KLayoutGraph into a Draw2D point.
+	 * @param kPoint KLayoutGraph point
+	 * @return Draw2D point
+	 */
+	private static Point kPoint2Point(KPoint kPoint)
+	{
+		Point point = new Point();
+		point.x = (int)kPoint.getX();
+		point.y = (int)kPoint.getY();
+		return point;
 	}
 	
 	/**
@@ -122,7 +237,8 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 					childNode.setParentGroup(parentGroup);
 					// set the child node's layout
 					KNodeGroupLayout nodeGroupLayout = KimlLayoutGraphFactory.eINSTANCE.createKNodeGroupLayout();
-					createLayout(nodeGroupLayout, boxEditPart);
+					createLayout(nodeGroupLayout, boxEditPart.getFigure());
+					nodeGroupLayout.setInsets(KimlLayoutGraphFactory.eINSTANCE.createKInsets());
 					childNode.setLayout(nodeGroupLayout);
 					// set the input and output ports
 					List subChildren = null;
@@ -144,8 +260,35 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 							port.setNodeGroup(childNode);
 							// set the port's layout
 							KPortLayout portLayout = KimlLayoutGraphFactory.eINSTANCE.createKPortLayout();
-							createLayout(portLayout, borderItem);
+							createLayout(portLayout, borderItem.getFigure());
 							port.setLayout(portLayout);
+							// set the port label
+							for (Object portChild : borderItem.getChildren())
+							{
+								if (portChild instanceof ITextAwareEditPart)
+								{
+									ITextAwareEditPart portNameEditPart = (ITextAwareEditPart)portChild;
+									KPortLabel portLabel = KimlLayoutGraphFactory.eINSTANCE.createKPortLabel();
+									portLabel.setText(portNameEditPart.getEditText());
+									port.setLabel(portLabel);
+									// set the port label's layout
+									KPortLabelLayout labelLayout = KimlLayoutGraphFactory.eINSTANCE.createKPortLabelLayout();
+									createLayout(labelLayout, portNameEditPart.getFigure());
+									portLabel.setLabelLayout(labelLayout);
+								}
+							}
+						}
+						else if (nodeElement instanceof ITextAwareEditPart)
+						{
+							ITextAwareEditPart boxNameEditPart = (ITextAwareEditPart)nodeElement;
+							// add node group label
+							KNodeGroupLabel nodeGroupLabel = KimlLayoutGraphFactory.eINSTANCE.createKNodeGroupLabel();
+							nodeGroupLabel.setText(boxNameEditPart.getEditText());
+							childNode.setLabel(nodeGroupLabel);
+							// set the node group label's layout
+							KNodeGroupLabelLayout labelLayout = KimlLayoutGraphFactory.eINSTANCE.createKNodeGroupLabelLayout();
+							createLayout(labelLayout, boxNameEditPart.getFigure());
+							nodeGroupLabel.setLabelLayout(labelLayout);
 						}
 					}
 					// process next hierarchy level
@@ -182,6 +325,9 @@ public class DataflowDiagramLayouter extends KimlAbstractLayouter {
 									sourcePort.getNodeGroup().getOutgoingEdges().add(edge);
 									targetPort.getEdges().add(edge);
 									targetPort.getNodeGroup().getIncomingEdges().add(edge);
+									// set the edge's layout
+									KEdgeLayout edgeLayout = KimlLayoutGraphFactory.eINSTANCE.createKEdgeLayout();
+									edge.setLayout(edgeLayout);
 								}
 							}
 						}
