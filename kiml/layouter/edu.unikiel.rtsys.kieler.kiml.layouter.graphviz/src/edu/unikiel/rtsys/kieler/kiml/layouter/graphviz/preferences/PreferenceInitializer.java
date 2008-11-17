162d4d54845309866f663c73f7184b17e9468c52
Arne Schipper <ars@informatik.uni-kiel.de>
2008-11-17 12:40:19 +0000

    set default enabled status of layout providers to 'true'

--- kiml/layouter/edu.unikiel.rtsys.kieler.kiml.layouter.graphviz/src/edu/unikiel/rtsys/kieler/kiml/layouter/graphviz/preferences/PreferenceInitializer.java
+++ kiml/layouter/edu.unikiel.rtsys.kieler.kiml.layouter.graphviz/src/edu/unikiel/rtsys/kieler/kiml/layouter/graphviz/preferences/PreferenceInitializer.java
@@ -1,5 +1,7 @@
 package edu.unikiel.rtsys.kieler.kiml.layouter.graphviz.preferences;
 
+import kiel.layouter.graphviz.GraphvizLayoutProviderNames;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
@@ -17,8 +19,17 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		
+		/* initialize the layout providers */
+		store.setDefault(GraphvizLayoutProviderNames.GRAPHVIZ_CIRCO, true);
+		store.setDefault(GraphvizLayoutProviderNames.GRAPHVIZ_DOT, true);
+		store.setDefault(GraphvizLayoutProviderNames.GRAPHVIZ_NEATO, true);
+		store.setDefault(GraphvizLayoutProviderNames.GRAPHVIZ_TWOPI, true);
+		
+		/* initialize defaults */
 		store.setDefault(PreferenceConstants.PREF_GRAPHVIZ_PADDING_X,15);
 		store.setDefault(PreferenceConstants.PREF_GRAPHVIZ_PADDING_Y,15);
+	
 	}
 
 }
--- kiml/layouter/edu.unikiel.rtsys.kieler.kiml.layouter.graphviz/src/kiel/layouter/graphviz/GraphvizLayoutProviderNames.java
+++ kiml/layouter/edu.unikiel.rtsys.kieler.kiml.layouter.graphviz/src/kiel/layouter/graphviz/GraphvizLayoutProviderNames.java
@@ -3,9 +3,9 @@ package kiel.layouter.graphviz;
 public class GraphvizLayoutProviderNames {
 
 	public final static String LAYOUT_PROVIDER_COLLECTION_ID = "GraphViz Collection";
-	final static String GRAPHVIZ_CIRCO = "GraphViz Circo";
-	final static String GRAPHVIZ_DOT = "GraphViz Dot";
-	final static String GRAPHVIZ_NEATO = "GraphViz Neato";
-	final static String GRAPHVIZ_TWOPI = "GraphViz Twopi";
+	public final static String GRAPHVIZ_CIRCO = "GraphViz Circo";
+	public final static String GRAPHVIZ_DOT = "GraphViz Dot";
+	public final static String GRAPHVIZ_NEATO = "GraphViz Neato";
+	public final static String GRAPHVIZ_TWOPI = "GraphViz Twopi";
 	
 }
