3e11a81164203af48556944c37aa2816c02d3233
Arne Schipper <ars@informatik.uni-kiel.de>
2008-11-19 11:18:40 +0000

    minor changes

--- kivik/edu.unikiel.rtsys.kieler.kivik/src/edu/unikiel/rtsys/kieler/kivik/internal/KivikComparator.java
+++ kivik/edu.unikiel.rtsys.kieler.kivik/src/edu/unikiel/rtsys/kieler/kivik/internal/KivikComparator.java
@@ -1,5 +1,10 @@
 package edu.unikiel.rtsys.kieler.kivik.internal;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Calendar;
 import java.util.Date;
@@ -7,8 +12,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.ResourceNode;
 import org.eclipse.compare.structuremergeviewer.ICompareInput;
+import org.eclipse.core.internal.resources.Workspace;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.compare.EMFComparePlugin;
 import org.eclipse.emf.compare.diff.metamodel.DiffFactory;
@@ -24,9 +33,18 @@ import org.eclipse.emf.compare.ui.EMFCompareUIMessages;
 import org.eclipse.emf.compare.ui.internal.ModelComparator;
 import org.eclipse.emf.compare.ui.util.EMFCompareConstants;
 import org.eclipse.emf.compare.util.EMFCompareMap;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.impl.EModelElementImpl;
+import org.eclipse.emf.ecore.impl.EcoreFactoryImpl;
+import org.eclipse.emf.ecore.impl.EcorePackageImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreAdapterFactory;
+import org.eclipse.emf.ecore.util.EcoreSwitch;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.gmf.runtime.emf.core.util.EMFCoreUtil;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.PlatformUI;
 
@@ -78,28 +96,37 @@ public final class KivikComparator {
 		return ModelComparator.getComparator(fCompareConfiguration);
 	}
 
-	private EObject getAncestorDomainEObject(){
+	private EObject getAncestorDomainEObject() {
 		if (getComparator().getAncestorResource() != null) {
-			ResourceSet rset = getComparator().getAncestorResource().getResourceSet();
+			ResourceSet rset = getComparator().getAncestorResource()
+					.getResourceSet();
 			return getDomainEObject(rset);
 		} else
 			return null;
 	}
-	
+
 	private EObject getLeftDomainEObject() {
 		ResourceSet rset = getComparator().getLeftResource().getResourceSet();
 		return getDomainEObject(rset);
 	}
-	
+
 	private EObject getRightDomainEObject() {
 		ResourceSet rset = getComparator().getRightResource().getResourceSet();
 		return getDomainEObject(rset);
 	}
-	
+
 	private EObject getDomainEObject(ResourceSet rset) {
 		EList<Resource> resources = rset.getResources();
 		if (resources.size() == 2) {
-			return resources.get(1).getContents().get(0);
+			Resource modelResource = resources.get(1);
+			try {
+				modelResource.load(null);
+				return modelResource.getContents().get(0);
+			} catch (IOException e) {
+				e.printStackTrace();
+				return null;
+			}
+
 		} else {
 			Resource diagramAndModelResource = resources.get(0);
 			return diagramAndModelResource.getContents().get(0);
@@ -132,70 +159,87 @@ public final class KivikComparator {
 	 * @return Result of the comparison of the loaded resources.
 	 */
 	public ModelInputSnapshot compareDomainModel() {
-		if (fKivikComparisonResult == null
-				|| fKivikComparisonResult.getDate() == null) {
-			fKivikComparisonResult = DiffFactory.eINSTANCE
+
+		final EObject ancestor = getAncestorDomainEObject();
+		final EObject left = getLeftDomainEObject();
+		final EObject right = getRightDomainEObject();
+
+		if (left != null && right != null) {
+			if (fKivikComparisonResult == null
+					|| fKivikComparisonResult.getDate() == null) {
+
+				fKivikComparisonResult = DiffFactory.eINSTANCE
+						.createModelInputSnapshot();
+				final Date start = Calendar.getInstance().getTime();
+
+				try {
+					PlatformUI.getWorkbench().getProgressService()
+							.busyCursorWhile(new IRunnableWithProgress() {
+								public void run(IProgressMonitor monitor)
+										throws InterruptedException {
+									final Map<String, Object> options = new EMFCompareMap<String, Object>();
+									options
+											.put(
+													MatchOptions.OPTION_PROGRESS_MONITOR,
+													monitor);
+
+									final MatchModel match;
+
+									if (ancestor == null)
+										match = MatchService.doContentMatch(
+												left, right, options);
+									else
+										match = MatchService.doContentMatch(
+												left, right, ancestor, options);
+									final DiffModel diff = DiffService.doDiff(
+											match,
+											getAncestorDomainEObject() != null);
+
+									fKivikComparisonResult.setDate(Calendar
+											.getInstance().getTime());
+									fKivikComparisonResult.setDiff(diff);
+									fKivikComparisonResult.setMatch(match);
+								}
+							});
+				} catch (InterruptedException e) {
+					fKivikComparisonResult.setDate(Calendar.getInstance()
+							.getTime());
+					fKivikComparisonResult.setDiff(DiffFactory.eINSTANCE
+							.createDiffModel());
+					fKivikComparisonResult.setMatch(MatchFactory.eINSTANCE
+							.createMatchModel());
+				} catch (InvocationTargetException e) {
+					EMFComparePlugin.log(e, true);
+				}
+
+				final Date end = Calendar.getInstance().getTime();
+				fCompareConfiguration.setProperty(
+						EMFCompareConstants.PROPERTY_COMPARISON_TIME, end
+								.getTime()
+								- start.getTime());
+				fCompareConfiguration.setLeftEditable(!getComparator()
+						.isLeftRemote());
+				fCompareConfiguration.setRightEditable(!getComparator()
+						.isRightRemote());
+				if (getComparator().isLeftRemote()) {
+					fCompareConfiguration.setLeftLabel(EMFCompareUIMessages
+							.getString("comparison.label.remoteResource")); //$NON-NLS-1$
+					fCompareConfiguration.setRightLabel(EMFCompareUIMessages
+							.getString("comparison.label.localResource")); //$NON-NLS-1$
+				}
+
+			}
+			return fKivikComparisonResult;
+
+		} else {
+			/* emergency result if one resource was null */
+			ModelInputSnapshot result = DiffFactory.eINSTANCE
 					.createModelInputSnapshot();
-			final Date start = Calendar.getInstance().getTime();
-
-			try {
-				PlatformUI.getWorkbench().getProgressService().busyCursorWhile(
-						new IRunnableWithProgress() {
-							public void run(IProgressMonitor monitor)
-									throws InterruptedException {
-								final Map<String, Object> options = new EMFCompareMap<String, Object>();
-								options.put(
-										MatchOptions.OPTION_PROGRESS_MONITOR,
-										monitor);
-
-								final MatchModel match;
-								if (getAncestorDomainEObject() == null)
-									match = MatchService.doContentMatch(
-											getLeftDomainEObject(),
-											getRightDomainEObject(), options);
-								else
-									match = MatchService.doContentMatch(
-											getLeftDomainEObject(),
-											getRightDomainEObject(),
-											getAncestorDomainEObject(),
-											options);
-								final DiffModel diff = DiffService.doDiff(
-										match,
-										getAncestorDomainEObject() != null);
-
-								fKivikComparisonResult.setDate(Calendar
-										.getInstance().getTime());
-								fKivikComparisonResult.setDiff(diff);
-								fKivikComparisonResult.setMatch(match);
-							}
-						});
-			} catch (InterruptedException e) {
-				fKivikComparisonResult
-						.setDate(Calendar.getInstance().getTime());
-				fKivikComparisonResult.setDiff(DiffFactory.eINSTANCE
-						.createDiffModel());
-				fKivikComparisonResult.setMatch(MatchFactory.eINSTANCE
-						.createMatchModel());
-			} catch (InvocationTargetException e) {
-				EMFComparePlugin.log(e, true);
-			}
-
-			final Date end = Calendar.getInstance().getTime();
-			fCompareConfiguration.setProperty(
-					EMFCompareConstants.PROPERTY_COMPARISON_TIME, end.getTime()
-							- start.getTime());
-			fCompareConfiguration.setLeftEditable(!getComparator()
-					.isLeftRemote());
-			fCompareConfiguration.setRightEditable(!getComparator()
-					.isRightRemote());
-			if (getComparator().isLeftRemote()) {
-				fCompareConfiguration.setLeftLabel(EMFCompareUIMessages
-						.getString("comparison.label.remoteResource")); //$NON-NLS-1$
-				fCompareConfiguration.setRightLabel(EMFCompareUIMessages
-						.getString("comparison.label.localResource")); //$NON-NLS-1$
-			}
+			result.setDate(Calendar.getInstance().getTime());
+			result.setDiff(DiffFactory.eINSTANCE.createDiffModel());
+			result.setMatch(MatchFactory.eINSTANCE.createMatchModel());
+			return result;
 		}
-		return fKivikComparisonResult;
 	}
 
 	public void loadResources(ICompareInput input) {
@@ -208,8 +252,8 @@ public final class KivikComparator {
 		 * .ssm_diagram to another EMF ModelComparator.
 		 */
 		// ResourceNode left = (ResourceNode) input.getLeft();
-		// ResourceNode right = input.getRight();
-		// ResourceNode ancestor = input.getAncestor();
+		// ResourceNode right = (ResourceNode) input.getRight();
+		// ResourceNode ancestor = (ResourceNode) input.getAncestor();
 		/*
 		 * And here's the trick. Emma has just the.ssm_diagram resources, as she
 		 * clicked on these files and these were loaded by the original EMF
