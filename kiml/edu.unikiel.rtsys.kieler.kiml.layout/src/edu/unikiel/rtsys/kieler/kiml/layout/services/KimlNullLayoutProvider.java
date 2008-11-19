33253dbb745d51e07e87939da5a1c2fabec1ba73
Arne Schipper <ars@informatik.uni-kiel.de>
2008-11-19 13:21:50 +0000

    added headers to files

--- kiml/edu.unikiel.rtsys.kieler.kiml.layout/src/edu/unikiel/rtsys/kieler/kiml/layout/services/KimlNullLayoutProvider.java
+++ kiml/edu.unikiel.rtsys.kieler.kiml.layout/src/edu/unikiel/rtsys/kieler/kiml/layout/services/KimlNullLayoutProvider.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Real-Time and Embedded Systems group
+ *
+ * INSERT LICENCE HERE
+ *
+ *
+ * Author: Arne Schipper, ars@informatik.uni-kiel.de 
+ *
+ *******************************************************************************/
 package edu.unikiel.rtsys.kieler.kiml.layout.services;
 
 import edu.unikiel.rtsys.kieler.kiml.layout.KimlLayoutGraph.KNodeGroup;
