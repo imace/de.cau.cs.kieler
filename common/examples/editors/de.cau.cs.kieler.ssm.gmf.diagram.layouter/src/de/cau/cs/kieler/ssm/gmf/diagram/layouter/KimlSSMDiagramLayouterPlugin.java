4af6ae3d2e525a74d1fe1faec9148a66f2e5ec19
Miro Spoenemann <msp@informatik.uni-kiel.de>
2009-01-19 17:52:52 +0000

    renamed package

---
+++ common/examples/editors/de.cau.cs.kieler.ssm.gmf.diagram.layouter/src/de/cau/cs/kieler/ssm/gmf/diagram/layouter/KimlSSMDiagramLayouterPlugin.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Real-Time and Embedded Systems group
+ *
+ * INSERT LICENCE HERE
+ *
+ *
+ * Author: Arne Schipper, ars@informatik.uni-kiel.de 
+ *
+ *******************************************************************************/
+package de.cau.cs.kieler.ssm.gmf.diagram.layouter;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * This activator class controls the life cycle of the SSM Diagram Layouter.
+ * 
+ * @author <a href="mailto:ars@informatik.uni-kiel.de">Arne Schipper</a>
+ * 
+ */
+public class KimlSSMDiagramLayouterPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.cau.cs.kieler.ssm.gmf.diagram.layouter";
+
+	// The shared instance
+	private static KimlSSMDiagramLayouterPlugin plugin;
+
+	/**
+	 * Constructs a new instance of this plug-in.
+	 */
+	public KimlSSMDiagramLayouterPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static KimlSSMDiagramLayouterPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
