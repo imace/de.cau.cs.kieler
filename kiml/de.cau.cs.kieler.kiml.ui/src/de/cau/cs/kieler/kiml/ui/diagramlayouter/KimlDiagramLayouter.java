f2a011accf15a4b66daab416cb20ee14ceda4e7b
Miro Spoenemann <msp@informatik.uni-kiel.de>
2009-02-11 13:23:17 +0000

    added error handling for diagram layouter

--- kiml/de.cau.cs.kieler.kiml.ui/src/de/cau/cs/kieler/kiml/ui/diagramlayouter/KimlDiagramLayouter.java
+++ kiml/de.cau.cs.kieler.kiml.ui/src/de/cau/cs/kieler/kiml/ui/diagramlayouter/KimlDiagramLayouter.java
@@ -9,8 +9,12 @@
  *******************************************************************************/
 package de.cau.cs.kieler.kiml.ui.diagramlayouter;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.statushandlers.StatusManager;
 
+import de.cau.cs.kieler.kiml.layout.KimlLayoutPlugin;
 import de.cau.cs.kieler.kiml.layout.services.KimlAbstractLayouterEngine;
 import de.cau.cs.kieler.kiml.layout.services.KimlRecursiveGroupLayouterEngine;
 import de.cau.cs.kieler.kiml.layout.services.LayoutListeners;
@@ -33,36 +37,42 @@ public final class KimlDiagramLayouter {
 	 *            IEditorPart or a StructuredSelection of EditParts
 	 */
 	public static void layout(Object target) {
-
-		// fetches editor id
-		String editorID = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-				.getActivePage().getActiveEditor().getEditorSite().getId();
-
-		// fetches layout graph builder for the provided editor
-		KimlAbstractLayoutGraphBuilder layoutGraphBuilder = LayoutGraphBuilders
-				.getInstance().getLayoutGraphBuilder(editorID);
-
-		// transforms the diagram into the KLayoutGraph and also use the mappings
-		KimlLayoutInformation layoutInformation = layoutGraphBuilder
-				.buildLayoutGraph(target);
-
-		// notifies layout listeners about the layout request
-		LayoutListeners.getInstance().layoutRequested(layoutInformation.layoutGraph);
-
-		// chooses the default layout engine
-		KimlAbstractLayouterEngine layoutEngine = new KimlRecursiveGroupLayouterEngine();
-
-		// does the layout with the layout graph
-		layoutEngine.layout(layoutInformation.layoutGraph);
-
-		// notifies layout listeners about the performed layout
-		LayoutListeners.getInstance().layoutPerformed(layoutInformation.layoutGraph);
-
-		// fetches layout graph applier for the provided editor
-		KimlAbstractLayoutGraphApplier layoutGraphApplier = LayoutGraphAppliers
-				.getInstance().getLayoutGraphAppliers(editorID);
-
-		// and applies layout to model
-		layoutGraphApplier.applyLayoutGraph(layoutInformation);
+		try {
+			// fetches editor id
+			String editorID = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getActivePage().getActiveEditor().getEditorSite().getId();
+	
+			// fetches layout graph builder for the provided editor
+			KimlAbstractLayoutGraphBuilder layoutGraphBuilder = LayoutGraphBuilders
+					.getInstance().getLayoutGraphBuilder(editorID);
+	
+			// transforms the diagram into the KLayoutGraph and also use the mappings
+			KimlLayoutInformation layoutInformation = layoutGraphBuilder
+					.buildLayoutGraph(target);
+	
+			// notifies layout listeners about the layout request
+			LayoutListeners.getInstance().layoutRequested(layoutInformation.layoutGraph);
+	
+			// chooses the default layout engine
+			KimlAbstractLayouterEngine layoutEngine = new KimlRecursiveGroupLayouterEngine();
+	
+			// does the layout with the layout graph
+			layoutEngine.layout(layoutInformation.layoutGraph);
+	
+			// notifies layout listeners about the performed layout
+			LayoutListeners.getInstance().layoutPerformed(layoutInformation.layoutGraph);
+	
+			// fetches layout graph applier for the provided editor
+			KimlAbstractLayoutGraphApplier layoutGraphApplier = LayoutGraphAppliers
+					.getInstance().getLayoutGraphAppliers(editorID);
+	
+			// and applies layout to model
+			layoutGraphApplier.applyLayoutGraph(layoutInformation);
+		}
+		catch (Throwable exception) {
+            Status status = new Status(IStatus.ERROR, KimlLayoutPlugin.PLUGIN_ID,
+                    "Failed to perform diagram layout.", exception);
+            StatusManager.getManager().handle(status, StatusManager.SHOW);
+        }
 	}
 }
