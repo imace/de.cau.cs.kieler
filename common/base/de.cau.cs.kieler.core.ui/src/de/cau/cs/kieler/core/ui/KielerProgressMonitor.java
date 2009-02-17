a54c5e035a00e84344197d2f26f1f41ad8fd6b2b
Miro Spoenemann <msp@informatik.uni-kiel.de>
2009-02-17 13:56:12 +0000

    added wrapper interfaces and implementations

---
+++ common/base/de.cau.cs.kieler.core.ui/src/de/cau/cs/kieler/core/ui/KielerProgressMonitor.java
@@ -0,0 +1,146 @@
+package de.cau.cs.kieler.core.ui;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
+
+/**
+ * Wrapper class for Eclipse progress monitors.
+ * 
+ * @author msp
+ */
+public class KielerProgressMonitor implements IKielerProgressMonitor {
+
+	/** the Eclipse progress monitor used by this monitor */
+	private IProgressMonitor progressMonitor;
+	/** the parent monitor */
+	private KielerProgressMonitor parentMonitor = null;
+	/** list of child monitors */
+	private List<IKielerProgressMonitor> children = new LinkedList<IKielerProgressMonitor>();
+	/** the number of work units that will be consumed after completion
+	 *  of the currently active child task */
+	private int currentChildWork = -1;
+	/** the start time of the associated task, in nanoseconds */
+	private long startTime;
+	/** the total time of the associated task, in seconds */
+	private double totalTime;
+	/** the name of the associated task */
+	private String taskName;
+	/** the amount of work that is completed */
+	private float completedWork = 0.0f;
+	/** the number of work units that were already submitted */
+	private int submittedWork = 0;
+	/** the number of work units that can be completed in total */
+	private int totalWork;
+
+	/**
+	 * Creates a progress monitor wrapper for a given Eclipse progress
+	 * monitor.
+	 * 
+	 * @param progressMonitor the progress monitor
+	 */
+	public KielerProgressMonitor(IProgressMonitor progressMonitor) {
+		this.progressMonitor = progressMonitor;
+	}
+	
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#begin(java.lang.String, int)
+	 */
+	public void begin(String name, int totalWork) {
+		this.taskName = name;
+		this.totalWork = totalWork;
+		if (parentMonitor == null)
+			progressMonitor.beginTask(name, totalWork <= 0
+					? IProgressMonitor.UNKNOWN : totalWork);
+		else
+			progressMonitor.subTask(name);
+		
+		startTime = System.nanoTime();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#done()
+	 */
+	public void done() {
+		totalTime = (System.nanoTime() - startTime) * 1e-9;
+		
+		if (completedWork < totalWork)
+			internalWorked(totalWork - completedWork);
+		if (parentMonitor == null)
+			progressMonitor.done();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#getExecutionTime()
+	 */
+	public double getExecutionTime() {
+		return totalTime;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#getSubMonitors()
+	 */
+	public List<IKielerProgressMonitor> getSubMonitors() {
+		return children;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#getTaskName()
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#isCanceled()
+	 */
+	public boolean isCanceled() {
+		return progressMonitor.isCanceled();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#subTask(int)
+	 */
+	public IKielerProgressMonitor subTask(int work) {
+		KielerProgressMonitor subMonitor = new KielerProgressMonitor(progressMonitor);
+		children.add(subMonitor);
+		subMonitor.parentMonitor = this;
+		currentChildWork = work;
+		return subMonitor;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.cau.cs.kieler.core.alg.IKielerProgressMonitor#worked(int)
+	 */
+	public void worked(int work) {
+		if (work > 0)
+			internalWorked(work);
+	}
+	
+	/**
+	 * Sets the current work counters of this monitor and all
+	 * parent monitors.
+	 * 
+	 * @param work amount of work that has been completed
+	 */
+	private void internalWorked(float work) {
+		if (totalWork > 0 && completedWork < totalWork) {
+			completedWork += work;
+			if (parentMonitor == null) {
+				int newWork = (int)completedWork;
+				if (newWork > submittedWork) {
+					progressMonitor.worked(newWork - submittedWork);
+					submittedWork = newWork;
+				}
+			}
+			else if (parentMonitor.currentChildWork > 0) {
+				 parentMonitor.internalWorked(work / totalWork
+						 * parentMonitor.currentChildWork);
+			}
+		}
+	}
+
+}
