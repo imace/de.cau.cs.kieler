49654491b1dde4c138fcd89205af2ebf4a275e62
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-03-24 17:43:38 +0000

    The parser project.

---
+++ common/examples/editors/de.cau.cs.kieler.ssm2/trunk/de.cau.cs.kieler.ssm2.dsl/src-gen/de/cau/cs/kieler/ssm2/dsl/ResourceFactoryRegistration.java
@@ -0,0 +1,9 @@
+package de.cau.cs.kieler.ssm2.dsl;
+
+import de.cau.cs.kieler.ssm2.dsl.resource.ssm2dlResourceFactory;
+
+public class ResourceFactoryRegistration {
+	public static void register() {
+	  	ssm2dlResourceFactory.register();
+	}
+}
