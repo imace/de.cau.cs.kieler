ded865f6a935fd31c29befcd9d0c63fd4635cda2
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-04-06 16:27:44 +0000

    The plug-in for the context menu. Currently, it is possible to add EntryActions via context menu; other actions and signals will follow, aswell as inner states.

---
+++ common/examples/editors/de.cau.cs.kieler.ssm2.custom.contextMenu/src/de/cau/cs/kieler/ssm2/custom/contextMenu/AddInnerActionAction.java
@@ -0,0 +1,21 @@
+package de.cau.cs.kieler.ssm2.custom.contextMenu;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IActionDelegate;
+
+public class AddInnerActionAction implements IActionDelegate {
+
+	@Override
+	public void run(IAction action) {
+		System.out.println("Selected OnInsideAction.");
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
