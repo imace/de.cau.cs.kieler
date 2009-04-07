56622f3a6297fb23bf3f93e511229b0db54035cb
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-04-07 14:25:49 +0000

    Now, SuspensionTriggers and Variables can be added through context menu, too.

--- common/examples/editors/de.cau.cs.kieler.ssm2.custom.contextMenu/src/de/cau/cs/kieler/ssm2/custom/contextMenu/AddVariable2Action.java
+++ common/examples/editors/de.cau.cs.kieler.ssm2.custom.contextMenu/src/de/cau/cs/kieler/ssm2/custom/contextMenu/AddVariable2Action.java
@@ -1,5 +1,74 @@
 package de.cau.cs.kieler.ssm2.custom.contextMenu;
 
-public class AddVariable2Action {
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartViewer;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.CompoundCommand;
+import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequest;
+import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewRequestFactory;
+import org.eclipse.gmf.runtime.diagram.ui.requests.RequestConstants;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IActionDelegate;
+
+import de.cau.cs.kieler.ssm2.diagram.edit.parts.State2EditPart;
+import de.cau.cs.kieler.ssm2.diagram.providers.Ssm2ElementTypes;
+
+public class AddVariable2Action implements IActionDelegate {
+
+	private IStructuredSelection currentSelection;
+	private State2EditPart selectedElement;
+	
+	@Override
+	public void run(IAction action) {
+		CompoundCommand cc = new CompoundCommand("Add Variable");
+
+		// Create the new Variable
+		CreateViewRequest variableRequest = CreateViewRequestFactory.getCreateShapeRequest(Ssm2ElementTypes.Variable_3010, selectedElement.getDiagramPreferencesHint());
+
+		//RegionEditPart regionEditPart = (RegionEditPart) selectedElement.getParent();
+		Command createVariableCmd = /*regionEditPart*/selectedElement.getCommand(variableRequest);
+		IAdaptable variableViewAdapter = (IAdaptable) ((List) variableRequest.getNewObject()).get(0);
+		
+		cc.add(createVariableCmd);
+
+		selectedElement.getDiagramEditDomain().getDiagramCommandStack().execute(cc);
+
+		// Put the new Signal in edit mode
+		final EditPartViewer viewer = selectedElement.getViewer();
+		final EditPart elementPart = (EditPart) viewer.getEditPartRegistry().get(variableViewAdapter.getAdapter(View.class));
+		if (elementPart != null) {
+			Display.getCurrent().asyncExec(new Runnable() {
+
+				public void run() {
+						viewer.setSelection(new StructuredSelection(elementPart));
+						Request der = new Request(RequestConstants.REQ_DIRECT_EDIT);
+						elementPart.performRequest(der);
+					}
+			});
+		}
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		try {
+			currentSelection = (IStructuredSelection) selection;
+			selectedElement = (State2EditPart) currentSelection.getFirstElement();
+			action.setEnabled(true);
+			return;
+		}
+		catch (Exception e) {
+			currentSelection = StructuredSelection.EMPTY;
+		}
+	}
 
 }
+
