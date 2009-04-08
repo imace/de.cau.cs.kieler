3c673658825d1356d8683718bf57dea50ef445d0
Miro Spoenemann <msp@informatik.uni-kiel.de>
2009-04-08 06:44:44 +0000

    removed core.services

--- common/base/de.cau.cs.kieler.core/src/de/cau/cs/kieler/core/services/GenericEventListener.java
+++
@@ -1,9 +0,0 @@
-package de.cau.cs.kieler.core.services;
-
-import java.util.EventListener;
-
-public interface GenericEventListener extends EventListener {
-
-	void handleEvent( GenericEvent e);
-	
-}
