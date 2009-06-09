0b9ac06611e0514d2552ccd440a2cdab6b446e8a
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-06-09 13:55:17 +0000

    Another try.

--- common/examples/editors/de.cau.cs.kieler.synccharts.custom.contextMenu/src/de/cau/cs/kieler/synccharts/custom/contextMenu/AddEntryActionHandler.java
+++ common/examples/editors/de.cau.cs.kieler.synccharts.custom.contextMenu/src/de/cau/cs/kieler/synccharts/custom/contextMenu/AddEntryActionHandler.java
@@ -4,11 +4,18 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class AddEntryActionHandler extends AbstractHandler implements IHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+		if (selection == null) {
+			selection = HandlerUtil.getCurrentSelection(event);
+		}
 		System.out.println("WOHOO!");
 		return null;
 	}
