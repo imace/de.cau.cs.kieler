8a6d1be05f687543b478a7d3432b7158b60a280b
Christian Motika <cmot@informatik.uni-kiel.de>
2009-06-21 15:17:27 +0000

    ptolemy model generation

---
+++ common/examples/editors/de.cau.cs.kieler.simplerailctrl.sim.ptolemy/src/de/cau/cs/kieler/simplerailctrl/sim/ptolemy/oaw/XtendJavaSystemOutput.java
@@ -0,0 +1,7 @@
+package de.cau.cs.kieler.simplerailctrl.sim.ptolemy.oaw;
+
+public class XtendJavaSystemOutput {
+	public final static void dump(String aString) {
+		System.out.println(aString);
+	}
+}
