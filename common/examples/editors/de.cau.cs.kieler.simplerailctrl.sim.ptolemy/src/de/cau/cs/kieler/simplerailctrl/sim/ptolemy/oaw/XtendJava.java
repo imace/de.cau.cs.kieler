2f5af6ce11ab44fffd08a6ca36dba2e422ede246
Christian Motika <cmot@informatik.uni-kiel.de>
2009-06-22 11:17:29 +0000

    transitions are set now

--- common/examples/editors/de.cau.cs.kieler.simplerailctrl.sim.ptolemy/src/de/cau/cs/kieler/simplerailctrl/sim/ptolemy/oaw/XtendJava.java
+++ common/examples/editors/de.cau.cs.kieler.simplerailctrl.sim.ptolemy/src/de/cau/cs/kieler/simplerailctrl/sim/ptolemy/oaw/XtendJava.java
@@ -1,17 +1,16 @@
 package de.cau.cs.kieler.simplerailctrl.sim.ptolemy.oaw;
 
 import java.util.Collection;
+import java.util.Hashtable;
 
 import simplerailctrl.*;
 import simplerailctrl.util.SimplerailctrlAdapterFactory;
 import org.openarchitectureware.*;
 import org.openarchitectureware.emf.*;
-
 import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.util.EcoreAdapterFactory;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-
 import de.cau.cs.kieler.simplerailctrl.provider.*;
 import simplerailctrl.*;
 
@@ -21,57 +20,25 @@ public class XtendJava {
 	}
 
 	public final static String hash(Node myNode) {
-		System.out.println(myNode.isInitial());
-		
-/*		
-		//EPackage pack = MetaModelRegistration.getEPackage();
+		return ""+myNode.hashCode();
+	}
+	public final static String hash(Transition myTransition) {
+		return ""+myTransition.hashCode();
+	}
 
-		int i = ((org.eclipse.emf.ecore.impl.DynamicEObjectImpl) myObject).hashCode();
-		System.out.println("1) "+myObject.eClass().getName());
-		
-
-		if (myObject.eClass().getName().equals("SetSpeed")) {
-			//Collection<EObject> myEObj = org.openarchitectureware.emf.EcoreUtil2.allContents(myObject);
-			//Object[] objArray = myEObj.toArray().clone();
-			//String name =(EObject)objArray[0]).eClass().getName();
-			
-			//EObject myObject2 = EcoreUtil.copy(myObject);
-			//System.out.println("2) "+myObject2.eClass().getName());
-			//(SetSpeed)myObject2.
-			//if (myObject2 instanceof Node) {
-			//	System.out.println(((SetSpeed)myObject2).getSpeed());
-			//}
-			
-			//SimplerailctrlAdapterFactory af = new SimplerailctrlAdapterFactory();
-			//EcoreAdapterFactory af2 = new EcoreAdapterFactory();
-			//SetSpeed setSpeed = (SetSpeed) af2.adapt(myObject, SetSpeed.class);
-			//SetSpeedItemProvider provider = new SetSpeedItemProvider(af);
-
-			//SetSpeed setSpeed = (SetSpeed) myObject;
-			//SetSpeed setSpeed = ((SetSpeed)((org.eclipse.emf.ecore.impl.DynamicEObjectImpl) myObject).dynamicGet(0));
-			//System.out.println("SPEEED"+setSpeed.getSpeed());
-		}
-		if (myObject.eClass().getName().equals("SetPoint")) {
-			
-		}
-		
-		//System.out.println("---->"+ ((NodeImpl)eObject).isInitial());
-/*		SetSignal setSignal;
-		SetSpeed setSpeed;
-		SetPoint setPoint;
-		if (node.eClass().isInstance(SetSpeed.class)) {
-			setSpeed = (SetSpeed) node;
-			System.out.println(setSpeed.toString());
-		}
-		if (node.eClass().isInstance(SetPoint.class)) { 
-			setPoint = (SetPoint) node;
-			System.out.println(setPoint.toString());
-		}
-		if (node.eClass().isInstance(SetPoint.class)) { 
-			setPoint = (SetPoint) node;
-			System.out.println(setPoint.toString());
-		}
-		*/
+	public final static String buildTrigger(Transition myTransition) {
 		return "";
+	}
+	public final static String buildEmission(Transition myTransition) {
+		return "";
+	}
+
+	static Hashtable ht = new Hashtable();
+	public final static void markState(Node myNode) {
+		ht.put(myNode.hashCode(), myNode);
+	}
+	
+	public final static boolean isMarked(Node myNode) {
+		return ht.contains(myNode);
 	}
 }
