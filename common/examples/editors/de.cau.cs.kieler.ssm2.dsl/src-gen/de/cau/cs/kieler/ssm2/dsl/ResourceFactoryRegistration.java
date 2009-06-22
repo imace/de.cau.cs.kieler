484b5c1a0d31ef6a94448a81be2548c67d1957a7
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-06-22 12:12:20 +0000

    deleted old ssm2 projectsh

--- common/examples/editors/de.cau.cs.kieler.ssm2.dsl/src-gen/de/cau/cs/kieler/ssm2/dsl/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package de.cau.cs.kieler.ssm2.dsl;
-
-import de.cau.cs.kieler.ssm2.dsl.resource.ssm2dlResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	ssm2dlResourceFactory.register();
-	}
-}
