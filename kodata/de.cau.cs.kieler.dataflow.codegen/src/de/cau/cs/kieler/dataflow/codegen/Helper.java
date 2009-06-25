1a13e61d0cc8f3d63b87db72368f21f11347a332
Claus Traulsen <ctr@informatik.uni-kiel.de>
2009-06-25 10:26:25 +0000

    improved code generation

--- kodata/de.cau.cs.kieler.dataflow.codegen/src/de/cau/cs/kieler/dataflow/codegen/Helper.java
+++ kodata/de.cau.cs.kieler.dataflow.codegen/src/de/cau/cs/kieler/dataflow/codegen/Helper.java
@@ -14,37 +14,107 @@
 
 package de.cau.cs.kieler.dataflow.codegen;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 
+import dataflow.Box;
+import dataflow.Connection;
 import dataflow.Port;
 
 public class Helper {
 
-	private static HashSet<String> defs = new HashSet<String>();
-	
-	public static boolean isDefined(Port p){
-		boolean res = defs.contains(p.getName());
-		if(!res){
-			defs.add(p.getName());
-		}
-		return res;
-	}
-	
-	public static boolean isDefined(String p){
-		boolean res = defs.contains(p);
-		if(!res){
-			defs.add(p);
-		}
-		return res;
-	}
-	
-	public static boolean huhu(){
-		return false;
-	}
-	
-	public static String timestamp() {
-		return String.valueOf( System.currentTimeMillis() );
-	}
+    private static HashSet<String> defs = new HashSet<String>();
+    private static HashSet<String> inputs = new HashSet<String>();
+    private static HashSet<String> outputs = new HashSet<String>();
+    private static HashSet<String> locals = new HashSet<String>();
+
+    // sample all ports that are targets
+    private static HashSet<Port> sources = new HashSet<Port>();
+    // sample all ports that are soures
+    private static HashSet<Port> targets = new HashSet<Port>();
+
+    private static HashMap<Port, String> port2Name = new HashMap<Port, String>();
+    private static HashMap<Port, Connection> port2Con = new HashMap<Port, Connection>();
+
+    /**
+     * Generate unique name for all connections by mapping target or source name
+     * to it. For inter-level transitions, the outermost port wins, otherwise
+     * the source-port.
+     * 
+     * @param box
+     */
+    public static void init(Box box) {
+        for (Connection c : box.getConnections()) {
+            String name = c.getSourcePort().getName();
+            if (port2Name.containsKey(c.getSourcePort())) {
+                name = port2Name.get(c.getSourcePort());
+            } else if (port2Name.containsKey(c.getTargetPort())) {
+                name = port2Name.get(c.getTargetPort());
+            }
+            port2Name.put(c.getSourcePort(), name);
+            port2Name.put(c.getTargetPort(), name);
+            port2Con.put(c.getSourcePort(), c);
+            port2Con.put(c.getTargetPort(), c);
+        }
+
+        for (Box b : box.getBoxes()) {
+            init(b);
+        }
+    }
+
+    public static void initIO(Box box) {
+        for (Connection c : box.getConnections()) {
+            sources.add(c.getSourcePort());
+            targets.add(c.getTargetPort());
+        }
+
+        for (Port p : box.getInputs()) {
+            if (!targets.contains(p)) {
+                inputs.add(p.getName());
+            } else {
+                locals.add(port2Name.get(p));
+            }
+        }
+        for (Port p : box.getOutputs()) {
+            if (!sources.contains(p)) {
+                outputs.add(p.getName());
+            }
+        }
+
+    }
+
+    public static Set<String> getInputs() {
+        return inputs;
+    }
+
+    public static Set<String> getOutputs() {
+        return outputs;
+    }
+
+    public static Set<String> getLocals() {
+        return locals;
+    }
+
+    public static Set<String> getLocals(Box box) {
+        HashSet<String> res = new HashSet<String>();
+        for (Box b : box.getBoxes()) {
+            for (Connection c : b.getConnections()) {
+                if (!box.getInputs().contains(c.getSourcePort())
+                        && !box.getOutputs().contains(c.getTargetPort())) {
+                    res.add(getName(c.getSourcePort()));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public static String timestamp() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    public static String getName(Port p) {
+        return port2Name.get(p);
+    }
 }
-
-
--- kodata/de.cau.cs.kieler.dataflow.codegen/src/de/cau/cs/kieler/dataflow/codegen/LustreGenerator.java
+++ kodata/de.cau.cs.kieler.dataflow.codegen/src/de/cau/cs/kieler/dataflow/codegen/LustreGenerator.java
@@ -28,59 +28,48 @@ import org.eclipse.ui.part.FileEditorInput;
 import org.openarchitectureware.workflow.WorkflowRunner;
 import org.openarchitectureware.workflow.monitor.NullProgressMonitor;
 
-import dataflow.Box;
-import dataflow.impl.BoxImpl;
-
 
 public class LustreGenerator extends AbstractHandler implements IHandler {
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.println(HandlerUtil.getCurrentSelection(event));
-		IEditorPart ed = HandlerUtil.getActiveEditor(event);
-		System.out.println(ed.getEditorInput().getName());
-		if(ed.isDirty()){
-		    ed.doSave(null);
-		}
-		
-		//String model = Activator.getDefault().  ed.getEditorInput().getName();
-		FileEditorInput uri = (FileEditorInput)ed.getEditorInput();
-                String model = "file:" + uri.getURI().getRawPath();
-            if(model.endsWith("_diagram")){
-                model=model.substring(0, model.length()-8);
-            }
-            System.out.println("model= "+ model);
-        //      BoxImpl b = new BoxImpl();
-	        Map<String,String> properties = new HashMap<String,String>();
-	        Map<String, Object> slotContents = new HashMap<String, Object>();
-	        
-	        
-	        System.out.println(Activator.getDefault().getStateLocation());
-	        properties.put("model", model);
-	        //properties.put("metamodel", MetaModel);
-	        properties.put("src-gen", ".") ;
-		
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IEditorPart ed = HandlerUtil.getActiveEditor(event);
+       /* if(ed.isDirty()){
+            ed.doSave(new NullProgressMonitor());
+        }*/
 
-                WorkflowRunner runner = new WorkflowRunner();  
-                
-                //System.out.println("Log to: " + runner.getWorkflowLogFileName());
-           String generator = "src/generator.oaw";
-            //generator = "/home/ctr/shared/kieler/trunk/kodata/de.cau.cs.kieler.dataflow.codegen/src/generator.oaw";
-		Log logger = LogFactoryImpl.getLog(runner.getClass());
-		logger.fatal("Test");
-                boolean success = runner.run(generator,
-		        new NullProgressMonitor(),    
-		            properties, 
-		            slotContents);  
-	
-		if(success){
-		    System.out.println("Code generation complete");
-		}else{
-		    System.out.println("Code generation failed");
-		}
-		
-		
-		return null;
-	}
+        FileEditorInput uri = (FileEditorInput)ed.getEditorInput();
+        String model = "file:" + uri.getURI().getRawPath();
+        if(model.endsWith("_diagram")){
+            model=model.substring(0, model.length()-8);
+        }
+        Map<String,String> properties = new HashMap<String,String>();
+        Map<String, Object> slotContents = new HashMap<String, Object>();
+
+
+        System.out.println(Activator.getDefault().getStateLocation());
+        properties.put("model", model);
+        properties.put("src-gen", ".") ;
+
+
+        WorkflowRunner runner = new WorkflowRunner();  
+
+        String generator = "src/generator.oaw";
+        Log logger = LogFactoryImpl.getLog(runner.getClass());
+        logger.fatal("Model=" + model);
+        boolean success = runner.run(generator,
+                new NullProgressMonitor(),    
+                properties, 
+                slotContents);  
+
+        if(success){
+            System.out.println("Code generation complete");
+        }else{
+            System.out.println("Code generation failed");
+        }
+
+
+        return null;
+    }
 
 }
