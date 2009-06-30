b14c4b99c19005325f8d27671866163b26b23258
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2009-06-30 15:30:19 +0000

    removed dir with wrong path

--- common/editors/de.cau.cs.kieler.ssm2.kit/de.cau.cs.kieler.ssm2.kit/src-gen/de/cau/cs/kieler/ssm2/kit/ResourceFactoryRegistration.java
+++
@@ -1,9 +0,0 @@
-package de.cau.cs.kieler.ssm2.kit;
-
-import de.cau.cs.kieler.ssm2.kit.resource.KITResourceFactory;
-
-public class ResourceFactoryRegistration {
-	public static void register() {
-	  	KITResourceFactory.register();
-	}
-}
