27447f07a0d062c175dfeb481a6b2fe8a2e40486
Christian Motika <cmot@informatik.uni-kiel.de>
2009-07-03 21:01:27 +0000

    galileo oAW Xpand code generation from within java

--- plugins/de.cau.cs.kieler.example.oaw.gen/src/de/cau/cs/kieler/example/oaw/handlers/SampleHandler.java
+++ plugins/de.cau.cs.kieler.example.oaw.gen/src/de/cau/cs/kieler/example/oaw/handlers/SampleHandler.java
@@ -6,11 +6,30 @@ import java.util.Map;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.openarchitectureware.workflow.WorkflowRunner;
+
+import org.eclipse.gmf.runtime.notation.Diagram;
+import mymodel.presentation.*;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.core.runtime.IAdaptable;
+
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.WorkflowContextDefaultImpl;
+import org.eclipse.emf.mwe.core.WorkflowFacade;
+import org.eclipse.emf.mwe.utils.Reader;
+import org.eclipse.emf.mwe.utils.Writer;
+import org.eclipse.xtend.typesystem.emf.EmfMetaModel;
+import org.eclipse.xpand2.Generator;
+import org.eclipse.xpand2.output.Outlet;
+import org.eclipse.xpand2.output.Output;
+import org.eclipse.emf.mwe.core.issues.*;
+import org.eclipse.emf.mwe.core.monitor.NullProgressMonitor;
+import org.eclipse.emf.mwe.internal.core.Workflow;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -18,55 +37,87 @@ import org.openarchitectureware.workflow.WorkflowRunner;
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class SampleHandler extends AbstractHandler {
+	
 	/**
 	 * The constructor.
 	 */
 	public SampleHandler() {
 	}
 
+	IResource extractSelection(ISelection sel) {
+		      if (!(sel instanceof IStructuredSelection))
+		         return null;
+		      IStructuredSelection ss = (IStructuredSelection) sel;
+		      Object element = ss.getFirstElement();
+		      if (element instanceof IResource)
+		         return (IResource) element;
+		      if (!(element instanceof IAdaptable))
+		         return null;
+		      IAdaptable adaptable = (IAdaptable)element;
+		      Object adapter = adaptable.getAdapter(IResource.class);
+		      return (IResource) adapter;
+	}
+	
 	/**
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-
-		//Code Generation Here//
-		String PluginRoot = SampleHandler.getPluginFolder();
-		String PluginModelRoot = SampleHandler.getPluginFolderModel();
-		String GenFolder = PluginRoot.replace("file:", "") + "src-gen";
+	    
+        // We need the DiagramEditor:
+        IEditorPart ep = HandlerUtil.getActiveEditor(event);
+        
+        ISelection selection = window.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+        
+        String inputModel = extractSelection(selection).getFullPath().toString();
+        
+        
+        System.out.println(inputModel);
+        
 		
-		Map<String,String> properties = new HashMap<String,String>();
-        Map<String, Object> slotContents = new HashMap<String, Object>();
+        //Workflow
+        Workflow workflow = new Workflow();
+        
+        //EMF reader
+        Reader emfReader = new Reader();
+        emfReader.setUri(inputModel);
+        emfReader.setModelSlot("model");
+        
+        //Meta model
+        EmfMetaModel metaModel = new EmfMetaModel(mymodel.MymodelPackage.eINSTANCE);
 
-        String WorkflowFile = "model/M2Tworkflow.oaw";
-		properties.put("model", PluginRoot + "My.mymodel");
-		properties.put("metamodel", PluginModelRoot + "model/mymodel.ecore");
-		properties.put("src-gen", GenFolder); 
-		
-		boolean success = false;
-		try {  
-		     WorkflowRunner runner = new WorkflowRunner();  
-		     success = runner.run(WorkflowFile, null,    
-		                                  properties, slotContents);  
-		} finally {}  
-			   
-        if (success) {
-    		MessageDialog.openInformation(
-    				window.getShell(),
-    				"Oaw Plug-in",
-    				"Code Generation sucessfull!");
-
-        }
-        else {
-    		MessageDialog.openInformation(
-    				window.getShell(),
-    				"Oaw Plug-in",
-					"Code Generation failed!");
-
-        }
-
-		
+        //Outlet
+        Outlet outlet = new Outlet();
+        outlet.setPath("c:\\");
+        
+        //Generator
+        Generator generator = new Generator();
+        generator.addMetaModel(metaModel);
+        generator.addOutlet(outlet);
+        
+        generator.setExpand("model::M2Ttemplate::main FOR model");
+        
+        WorkflowContext wfx = new WorkflowContextDefaultImpl();
+        Issues issues = new org.eclipse.emf.mwe.core.issues.IssuesImpl();
+        NullProgressMonitor monitor = new NullProgressMonitor();
+        
+        workflow.addComponent(emfReader);
+        workflow.addComponent(generator);
+        workflow.invoke(wfx, monitor, issues);
+        
+        System.out.print(generator.getLogMessage());
+        
+        System.out.print(issues.getInfos());
+        System.out.print(issues.getIssues());
+        System.out.print(issues.getWarnings());
+        System.out.print(issues.getErrors().toString());
+        
+        //EMF writer
+        //Writer emfWriter = new Writer();
+        //emfWriter.setUri(modelURI);
+        //emfWriter.setModelSlot("model");        
+        
 		return null;
 	}
 	
@@ -75,7 +126,7 @@ public class SampleHandler extends AbstractHandler {
 	private static String _pluginFolder;
 	public static String getPluginFolder() {
 	        if(_pluginFolder == null) {
-	            _pluginFolder = Platform.getBundle("de.cau.cs.kieler.example.oaw.gen").getLocation();
+//	            _pluginFolder = Platform.getBundle("de.cau.cs.kieler.example.oaw.gen").getLocation();
 	            _pluginFolder = _pluginFolder.replace("initial@reference:", "");
 	            _pluginFolder = _pluginFolder.replace("reference:", "");
 	            _pluginFolder = _pluginFolder.replace("file:/", "");
@@ -86,7 +137,7 @@ public class SampleHandler extends AbstractHandler {
 	private static String _pluginFolderModel;
 	public static String getPluginFolderModel() {
 	        if(_pluginFolderModel == null) {
-	        	_pluginFolderModel = Platform.getBundle("de.cau.cs.kieler.example.oaw").getLocation();
+//	        	_pluginFolderModel = Platform.getBundle("de.cau.cs.kieler.example.oaw").getLocation();
 	        	_pluginFolderModel = _pluginFolderModel.replace("initial@reference:", "");
 	        	_pluginFolderModel = _pluginFolderModel.replace("reference:", "");
 	        	_pluginFolderModel = _pluginFolderModel.replace("file:/", "");
