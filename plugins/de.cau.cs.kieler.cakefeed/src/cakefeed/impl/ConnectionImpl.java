4601304d5445cb434164303d894b0d14c8b561b6
Matthias Schmeling <schm@informatik.uni-kiel.de>
2009-11-02 23:50:38 +0000

    Added reference to state charts in basic function blocks.

---
+++ plugins/de.cau.cs.kieler.cakefeed/src/cakefeed/impl/ConnectionImpl.java
@@ -0,0 +1,200 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package cakefeed.impl;
+
+import cakefeed.Application;
+import cakefeed.CakefeedPackage;
+import cakefeed.Connection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Connection</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link cakefeed.impl.ConnectionImpl#getParentApplication <em>Parent Application</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ConnectionImpl extends EObjectImpl implements Connection {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ConnectionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return CakefeedPackage.Literals.CONNECTION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Application getParentApplication() {
+		if (eContainerFeatureID() != CakefeedPackage.CONNECTION__PARENT_APPLICATION) return null;
+		return (Application)eContainer();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetParentApplication(Application newParentApplication, NotificationChain msgs) {
+		msgs = eBasicSetContainer((InternalEObject)newParentApplication, CakefeedPackage.CONNECTION__PARENT_APPLICATION, msgs);
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setParentApplication(Application newParentApplication) {
+		if (newParentApplication != eInternalContainer() || (eContainerFeatureID() != CakefeedPackage.CONNECTION__PARENT_APPLICATION && newParentApplication != null)) {
+			if (EcoreUtil.isAncestor(this, newParentApplication))
+				throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
+			NotificationChain msgs = null;
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			if (newParentApplication != null)
+				msgs = ((InternalEObject)newParentApplication).eInverseAdd(this, CakefeedPackage.APPLICATION__CONNECTIONS, Application.class, msgs);
+			msgs = basicSetParentApplication(newParentApplication, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, CakefeedPackage.CONNECTION__PARENT_APPLICATION, newParentApplication, newParentApplication));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				if (eInternalContainer() != null)
+					msgs = eBasicRemoveFromContainer(msgs);
+				return basicSetParentApplication((Application)otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				return basicSetParentApplication(null, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eBasicRemoveFromContainerFeature(NotificationChain msgs) {
+		switch (eContainerFeatureID()) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				return eInternalContainer().eInverseRemove(this, CakefeedPackage.APPLICATION__CONNECTIONS, Application.class, msgs);
+		}
+		return super.eBasicRemoveFromContainerFeature(msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				return getParentApplication();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				setParentApplication((Application)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				setParentApplication((Application)null);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case CakefeedPackage.CONNECTION__PARENT_APPLICATION:
+				return getParentApplication() != null;
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //ConnectionImpl
