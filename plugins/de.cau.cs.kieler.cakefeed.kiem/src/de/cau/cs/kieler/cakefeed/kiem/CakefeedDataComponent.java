a280e4990304215d36b5f27ff7e01658366cbeca
Matthias Schmeling <schm@informatik.uni-kiel.de>
2010-02-11 03:38:48 +0000

    Some progress with simulation.

--- plugins/de.cau.cs.kieler.cakefeed.kiem/src/de/cau/cs/kieler/cakefeed/kiem/CakefeedDataComponent.java
+++ plugins/de.cau.cs.kieler.cakefeed.kiem/src/de/cau/cs/kieler/cakefeed/kiem/CakefeedDataComponent.java
@@ -1,10 +1,17 @@
 package de.cau.cs.kieler.cakefeed.kiem;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.parts.IDiagramWorkbenchPart;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -57,11 +64,6 @@ public class CakefeedDataComponent extends JSONObjectDataComponent implements IJ
 
 	@Override
 	public JSONObject step(JSONObject jSONObject) throws KiemExecutionException {
-		// TODO Auto-generated method stub
-		// gucken, welcher tick ausgewaehlt ist
-		// entsprechenden tick aus trace variable raussuchen
-		// events/vars/connections aus tick highlighten
-		System.out.println("STEP START");
 		if (!isHistoryStep()) {
 			currentTick++;
 		} else if (currentTick > 0) {
@@ -69,12 +71,59 @@ public class CakefeedDataComponent extends JSONObjectDataComponent implements IJ
 		}
 		Element tick = getTick(currentTick, trace.getDocumentElement());
 		if (tick != null) {
-			
+			NodeList children = tick.getChildNodes();
+			for (int i = 0; i < children.getLength(); i++) {
+				Node child = children.item(i);
+				if (child instanceof Element) {
+					Element e = (Element)child;
+					if (e.getNodeName().equals("Component") && e.hasAttribute("Name")) {
+						String fBName = e.getAttribute("Name");
+						List<String> signalNames = getChildrenNames(e, "Signal");
+						List<String> sourceStateNames = getChildrenNames(e, "SourceState");
+						List<String> destinationStateNames = getChildrenNames(e, "DestinationState");
+						highlightSignals(fBName, signalNames);
+						highlightTransitions(fBName, sourceStateNames, destinationStateNames);
+					}
+				}
+			}
 		}
-		System.out.println("STEP FINISH");
 		return null;
 	}
 	
+	private void highlightTransitions(String fBName,
+			List<String> sourceStateNames, List<String> destinationStateNames) {
+		// TODO Auto-generated method stub
+		IEditorPart activeEditor = PlatformUI.getWorkbench().getWorkbenchWindows()[0].getActivePage().getActiveEditor();
+		if (activeEditor instanceof IDiagramWorkbenchPart) {
+			EObject obj = ((View) ((IDiagramWorkbenchPart) activeEditor).getDiagramEditPart()
+        				.getModel()).getElement();
+			System.out.println("WINDOW");
+		}
+	}
+
+	private void highlightSignals(String fBName, List<String> signalNames) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private List<String> getChildrenNames(Element elem, String type) {
+		List<String> names = new ArrayList<String>();
+		NodeList children = elem.getChildNodes();
+		for (int i = 0; i < children.getLength(); i++) {
+			Node child = children.item(i);
+			if (child instanceof Element) {
+				Element e = (Element)child;
+				if (e.getNodeName().equals(type)) {
+					names.add(e.getTextContent());
+				} 
+				else {
+					names.addAll(getChildrenNames(e, type));
+				}
+			}
+		}
+		return names;
+	}
+
 	private Element getTick(int tick, Element elem) {
 		String name = elem.getNodeName();
 		if (name.equals("Tick") && elem.hasAttribute("Id") && (elem.getAttribute("Id").equals(""+ tick +""))) {
