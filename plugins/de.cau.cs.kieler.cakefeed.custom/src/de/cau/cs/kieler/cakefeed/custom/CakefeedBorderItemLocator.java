7f77087e2046ca98e0a79519b3eb4ff405fbfd10
Matthias Schmeling <schm@informatik.uni-kiel.de>
2010-04-28 00:30:48 +0000

    Bug fixing.

--- plugins/de.cau.cs.kieler.cakefeed.custom/src/de/cau/cs/kieler/cakefeed/custom/CakefeedBorderItemLocator.java
+++ plugins/de.cau.cs.kieler.cakefeed.custom/src/de/cau/cs/kieler/cakefeed/custom/CakefeedBorderItemLocator.java
@@ -1,6 +1,7 @@
 package de.cau.cs.kieler.cakefeed.custom;
 
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gmf.runtime.diagram.ui.figures.BorderItemLocator;
 import org.eclipse.gmf.runtime.gef.ui.figures.DefaultSizeNodeFigure;
@@ -52,4 +53,41 @@ public class CakefeedBorderItemLocator extends BorderItemLocator {
 		}
 		return newLocation;
 	}
+	
+	@Override
+	protected Point locateOnParent(Point suggestedLocation, int suggestedSide,
+			IFigure borderItem) {
+		Point newPoint = super.locateOnParent(suggestedLocation, suggestedSide, borderItem);
+		Rectangle newBounds = new Rectangle();
+		newBounds.x = newPoint.x;
+		newBounds.y = newPoint.y;
+		newBounds.width = borderItem.getPreferredSize().width;
+		newBounds.height = borderItem.getPreferredSize().height;
+		newBounds = getValidLocation2(newBounds, borderItem);
+		return new Point(newBounds.x, newBounds.y);
+	}
+
+	private Rectangle getValidLocation2(Rectangle newBounds, IFigure borderItem) {
+		Rectangle newLocation = newBounds;
+		if (fBTypeFigure != null) {
+			Rectangle reference = null;
+			if (ctrl) {
+				reference = fBTypeFigure.getCtrlRectangle();
+			} else {
+				reference = fBTypeFigure.getDataRectangle();
+			}
+			if (newLocation.y < reference.y) {
+				newLocation.y = reference.y;
+			} else if ((newLocation.y + newLocation.height) > (reference.y + reference.height)) {
+				newLocation.y = reference.y + reference.height - newLocation.height;
+			}
+			if (input && ((newLocation.x + newLocation.width) > reference.x)) {
+				newLocation.x = reference.x - newLocation.width;
+			} else if (!input && (newLocation.x < (reference.x + reference.width))) {
+				newLocation.x = newLocation.x = (reference.x + reference.width);
+			}
+		}
+		return newLocation;
+	}
+	
 }
