5ba9c012d33a8b35a8f5989b5c2993b7dcfd21e8
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-07-07 13:02:07 +0000

    added viewmanagement effects to copy and paste

--- plugins/de.cau.cs.kieler.viewmanagement.effects/src/de/cau/cs/kieler/viewmanagement/effects/LayoutEffect.java
+++ plugins/de.cau.cs.kieler.viewmanagement.effects/src/de/cau/cs/kieler/viewmanagement/effects/LayoutEffect.java
@@ -17,9 +17,12 @@ package de.cau.cs.kieler.viewmanagement.effects;
 import org.eclipse.gef.EditPart;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 import de.cau.cs.kieler.core.ui.util.EditorUtils;
+import de.cau.cs.kieler.kiml.ui.layout.DiagramLayoutManager;
 import de.cau.cs.kieler.kiml.ui.layout.EclipseLayoutServices;
 import de.cau.cs.kieler.viewmanagement.AEffect;
 
@@ -31,36 +34,39 @@ import de.cau.cs.kieler.viewmanagement.AEffect;
  */
 public class LayoutEffect extends AEffect {
 
-//    EditPart editPart; // the edit part for the auto layout method
+    // EditPart editPart; // the edit part for the auto layout method
     IEditorPart editorPart; // the editor part for the auto layout method
 
     /**
      * Performs auto layout by simply calling the layout method from
      * {@link DiagramLayoutManager}. It must be called in a safe thread, because
-     * it changes the notation model and hence must go through the EMF transaction
-     * mechanism. When this execute is called from a weird thread, e.g. when
-     * the notation model is in a read-only context, you might otherwise get an
-     * IllegalStateException. So here layout is called from within the 
-     * display thread.
+     * it changes the notation model and hence must go through the EMF
+     * transaction mechanism. When this execute is called from a weird thread,
+     * e.g. when the notation model is in a read-only context, you might
+     * otherwise get an IllegalStateException. So here layout is called from
+     * within the display thread.
      */
     @Override
     public final void execute() {
         final IWorkbench workbench = PlatformUI.getWorkbench();
         workbench.getDisplay().syncExec(new Runnable() {
-          public void run() {
-              if(editorPart == null){
-                  // if no editor is set, the active editor will be taken.
-                  editorPart = EditorUtils.getLastActiveEditor();
-              }
-              EclipseLayoutServices.getInstance().layout(editorPart, null, true, false);
-          } } );
+            public void run() {
+                if (editorPart == null) {
+                    // if no editor is set, the active editor will be taken.
+                    editorPart = EditorUtils.getLastActiveEditor();
+                }
+                EclipseLayoutServices.getInstance().layout(editorPart, null,
+                        true, false);
+            }
+        });
     }
 
     /**
      * Sets the effect parameters. In this special case the parameter is an
      * IEditorPart
      */
- 
+
+    @Override
     public final void setParameters(final Object parameters) {
         if (parameters instanceof IEditorPart) {
             this.editorPart = (IEditorPart) parameters;
@@ -72,8 +78,14 @@ public class LayoutEffect extends AEffect {
      */
     @Override
     public final void setTarget(final EditPart target) {
-        this.editorPart=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        IWorkbenchWindow window = PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow();
+        if (window != null) {
+            IWorkbenchPage page = window.getActivePage();
+            if (page != null) {
+                this.editorPart = page.getActiveEditor();
+            }
+        }
     }
 
 }
-
